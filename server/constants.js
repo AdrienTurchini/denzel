8f92a8f1060d327b97cbb7cbd402a49d379ab252
Adrien Turchini <adrien.turchini@me.com>
2020-03-29 16:16:48 +0200

    setup API using expressjs, sandbox create a json file with data and /movies/populate/:id import the json file using curl -d denzelMovies.json

--- server/constants.js
+++ server/constants.js
@@ -3,11 +3,16 @@ const IMDB_TITLE_URL = 'https://www.imdb.com/title';
 const IMDB_URL = 'https://www.imdb.com';
 const P_LIMIT = 25;
 const PORT = '9292';
+const CONNECTION_URL = "mongodb+srv://admin:admin@adrix-rplru.mongodb.net/test?retryWrites=true&w=majority";
+const DATABASE_NAME = "movies";
+
 
 module.exports = {
   IMDB_NAME_URL,
   IMDB_TITLE_URL,
   IMDB_URL,
   P_LIMIT,
-  PORT
+  PORT,
+  CONNECTION_URL,
+  DATABASE_NAME
 };
--- server/index.js
+++ server/index.js
@@ -1,21 +1,133 @@
-const cors = require('cors');
+    const cors = require('cors');
 const express = require('express');
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
 const helmet = require('helmet');
 const {PORT} = require('./constants');
+const {DATABASE_NAME} = require('./constants');
+const {CONNECTION_URL} = require('./constants');
 
 const app = express();
 
 module.exports = app;
 
-app.use(require('body-parser').json());
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 app.use(helmet());
 
 app.options('*', cors());
 
-app.get('/', (request, response) => {
-  response.send({'ack': true});
+var database, collection;
+
+app.listen(PORT, () => {
+  MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+      if(error) {
+          throw error;
+      }
+      database = client.db(DATABASE_NAME);
+      collection = database.collection("denzel");
+      console.log("Connected to " + DATABASE_NAME + "!");
+      console.log(`📡 Running on port ${PORT}`);
+  });
 });
 
-app.listen(PORT);
-console.log(`📡 Running on port ${PORT}`);
+/**
+* Populate the database with the movies from the imdb actor's id
+*/
+app.get("/movies/populate/:id", (request, response) => {
+    const id = request.params.id;
+  collection.insert(request.body, (error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+        nb = result.insertedCount;
+        response.send({"Total of documents inserted " : nb});
+  });
+});
+
+/**
+ * Fetch a random must-watch movie (metascore > 70)
+ */
+app.get("/movies", (request, response) => {
+        var query = {metascore: {$gt: 70}};
+        collection.aggregate([{$match:query},{$sample: {size: 1}}]).toArray((error, result) => {
+            if(error) {
+                return response.status(500).send(error);
+            }
+            response.send(result);     
+        });
+});
+
+/**
+ * Fetch a specific movie using the id after /movies
+ * Or search for all Denzel movies with optional parameters limit and metascore after using /search?limit=XX&metascore=XX after /movies, without the parameters, limit = 5 and metascore = 0 by default
+ * the data are sorted decreasly by metascore
+ */
+app.get("/movies/:parameter", (request, response) => {
+    
+    param = request.params.parameter;
+
+    var _limit = request.param('limit');
+    var limitParse = parseInt(_limit);
+
+    var _metascore = request.param('metascore'); 
+    var metascoreParse = parseInt(_metascore);
+    
+    if(metascoreParse >= 0 && metascoreParse <= 100) {
+        // nothing to do, value is correct 
+    }
+    else {
+        metascoreParse = 0;
+    }
+
+    if(limitParse >= 0) {
+        // nothing to do, value is correct 
+    }
+    else {
+        limitParse = 5;
+    }
+
+    var query = {metascore: {$gt: metascoreParse}};
+    
+    if(param == 'search')
+    {
+        collection.find(query).sort({metascore: -1}).limit(limitParse).toArray((error, result) => {
+            if(error) {
+                return response.status(500).send(error);
+            }
+            response.send(result);
+        });
+    }
+    else {
+        collection.findOne({ "id": request.params.parameter}, (error, result) => {
+            if(error) {
+                return response.status(500).send(error);
+            }
+            response.send(result);
+        });
+    }
+});
+
+app.post("/movies/:id", (request, response) => {
+    const idMovie = request.params.id;
+    var myReviews = request.body;
+
+
+    var query = {id: idMovie};
+    var update = {$push: {myReviews}};
+
+    collection.update(query, update, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+
+
+ 
+
